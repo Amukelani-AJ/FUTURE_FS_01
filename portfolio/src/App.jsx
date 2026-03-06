7891270dfcbc143e7be3f5873c0e8afeb5d0f2b7
Amukelani-AJ <ndlovuamukelanij@gmail.com>
2026-03-06 13:49:25 +0200

    Adde routing for all the components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,22 @@
 //import { useState } from 'react'
-
 import './App.css'
+import { Project } from './components/Project';
+import { About } from './components/About';
+import { HomePage } from './components/HomePage';
+import { Contact } from './components/Contact';
+
 
 function App() {
 
 
   return (
     <>
-      <div>
-        
-      </div>
+      <Routes>
+        <Route index element={<HomePage/>} />
+        <Route path="Project" element={<Project/>} />
+        <Route path="About" element={<About/>} />
+        <Route path="Contact" element={<Contact/>} />
+      </Routes>
     </>
   )
 }
