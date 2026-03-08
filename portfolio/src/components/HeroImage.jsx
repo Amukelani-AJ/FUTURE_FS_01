ef30d0041ef299023dc92d3754094b31145b9b5a
Amukelani-AJ <ndlovuamukelanij@gmail.com>
2026-03-08 12:33:36 +0200

    Added images to assets

---
+++ portfolio/src/components/HeroImage.jsx
@@ -0,0 +1,9 @@
+import './HeroImage.css';
+
+export function HeroImage(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
--- portfolio/src/pages/HomePage.jsx
+++ portfolio/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { NavBar } from "../components/NavBar";
+import { HeroImage} from "../components/HeroImage";
 
 export function HomePage(){
     return(
         <div>
             <NavBar/>
+            <HeroImage/>
         </div>
     );
 }
