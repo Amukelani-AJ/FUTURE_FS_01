de83571c347fcd5f8d74644b0eb68c12a2bfb212
Amukelani-AJ <ndlovuamukelanij@gmail.com>
2026-03-10 16:24:43 +0200

    Created the form and form.css

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,12 +1,14 @@
 import { NavBar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
 import { HeroImage2 } from "../components/HeroImage2";
+import { Form } from "../components/Form";
 
 export function Contact() {
   return (
     <div>
       <NavBar />
       <HeroImage2 heading="CONTACT" text="Contact me via email" />
+      <Form/>
       <Footer />
     </div>
   );
