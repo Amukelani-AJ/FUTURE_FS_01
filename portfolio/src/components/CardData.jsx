5d88460aeefa4c87d3a49879399b0a2502d79d17
Amukelani-AJ <ndlovuamukelanij@gmail.com>
2026-03-13 17:08:57 +0200

    Added projects

--- portfolio/src/components/CardData.jsx
+++ portfolio/src/components/CardData.jsx
@@ -1,5 +1,4 @@
 import project1 from "../assets/project1.png";
-import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
 
 export const CardData = [
@@ -9,16 +8,17 @@ export const CardData = [
     text: "This is a basic chatbot that i used to pactice react following a youtube tutoria",
     view: "url.com",
   },
+
   {
-    imgsrc: project2,
-    title: "Chatbot Project",
-    text: "This is a basic chatbot that i used to pactice react following a youtube tutoria",
+    imgsrc: project3,
+    title: "mini CRM",
+    text: "Internship task 2 (In progress.....): Building a CRM to manage client leads generated from website contact forms",
     view: "url.com",
   },
   {
     imgsrc: project3,
-    title: "Chatbot Project",
-    text: "This is a basic chatbot that i used to pactice react following a youtube tutoria",
+    title: "Local Business Website",
+    text: "Internship task 3 (In progress.....):Building a professional website for a real local business.",
     view: "url.com",
   },
 ];
