8320347c3287c212bdb550417045f744c48bf684
Amukelani-AJ <ndlovuamukelanij@gmail.com>
2026-03-13 23:55:40 +0200

    removed chatbot project

--- src/components/CardData.jsx
+++ src/components/CardData.jsx
@@ -1,13 +1,7 @@
-import project1 from "../assets/project1.png";
+
 import project3 from "../assets/project3.png";
 
 export const CardData = [
-  {
-    imgsrc: project1,
-    title: "Chatbot Project",
-    text: "This is a basic chatbot that i used to pactice react following a youtube tutoria",
-    view: "url.com",
-  },
 
   {
     imgsrc: project3,
@@ -18,7 +12,7 @@ export const CardData = [
   {
     imgsrc: project3,
     title: "Local Business Website",
-    text: "Internship task 3 (In progress.....):Building a professional website for a real local business.",
+    text: "Internship task 3 (In progress.....): Building a professional website for a real local business.",
     view: "url.com",
   },
 ];
