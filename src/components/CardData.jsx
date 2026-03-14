d673dd1e34225e52b009bd263b8d6b933544a4a5
Amukelani-AJ <ndlovuamukelanij@gmail.com>
2026-03-14 13:26:07 +0200

    Removed default urls

--- src/components/CardData.jsx
+++ src/components/CardData.jsx
@@ -1,18 +1,16 @@
-
 import project3 from "../assets/project3.png";
 
 export const CardData = [
-
   {
     imgsrc: project3,
     title: "mini CRM",
     text: "Internship task 2 (In progress.....): Building a CRM to manage client leads generated from website contact forms",
-    view: "url.com",
+    view: "",
   },
   {
     imgsrc: project3,
     title: "Local Business Website",
     text: "Internship task 3 (In progress.....): Building a professional website for a real local business.",
-    view: "url.com",
+    view: "",
   },
 ];
